3925975bd8036cdadf054d342ec28fe7a95a3f35
M Hassaan Awan <96558549+HassaanAwan786@users.noreply.github.com>
2024-10-27 20:59:26 +0500

    Passed

--- chanukah.cpp
+++ chanukah.cpp
@@ -1,23 +1,29 @@
-#include <iostream>
-using namespace std;
-#include <iostream>
-using namespace std;
 
-int main() {
-
-    long num;
-    long long k, days, sum = 0;
-    cin >> num;
-    for (int l = 0; l < num; l++)
+ Download chanukah.cpp
+#include<iostream>
+using namespace std;
+typedef long long ll;
+ll sum(int n)
+{
+    ll x = 2, answer = 0;
+    for (int i = 0; i < n; i++)
     {
-        cin >> k >> days;
-        for (int m = days; m > 0; m--)
-        {
-            sum = sum + m + 1;
-        }
-
-        cout << l + 1 << " " << sum;
-        sum = 0;
+        answer += x;
+        x++;
+    }
+    return answer;
+}
+int main()
+{
+    int t;
+    cin >> t;
+    int num = 0, days = 0;
+    ll answer = 0;
+    while(t--)
+    {
+        cin >> num >> days;
+        answer = sum(days);
+        cout << num << " " << answer << endl;
     }
     return 0;
 }
