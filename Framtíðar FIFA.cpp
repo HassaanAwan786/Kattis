faf9fc7b1c630498c9a4a66a38867f46c729a43a
M Hassaan Awan <96558549+HassaanAwan786@users.noreply.github.com>
2024-10-27 21:22:23 +0500

    Compiler error

---
+++ Framtíðar FIFA.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+in main()
+{
+    long year="2022",yearimp,rimp;
+    cin>>rimp;
+    cout<<endl;
+    cin>>yearimp;
+    cout<<year+(rimp/yearimp);
+return 0;
+
+}
