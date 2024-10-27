b3eaa5c28c16c0e2773bdefb19067e617899d1c4
M Hassaan Awan <96558549+HassaanAwan786@users.noreply.github.com>
2024-10-27 21:31:03 +0500

    compiler error

--- Sierpiński Circumference.cpp
+++ Sierpiński Circumference.cpp
@@ -1,28 +1,12 @@
+ #include <iostream>
+#include <algorithm>  
 using namespace std;
-int main() {
-    int num = 1;
 
-    while (true) {
-        string line, stringRes;
-        getline(cin, line);
-        if (cin.eof()) { break; }
-        long double shrinks = atof(line.c_str());
-
-        long double triangels = pow(3, shrinks);
-        long double length = 3 / pow(2, shrinks);
-        long double res = floor(triangels * length * 3);
-        int i = 0;
-        while (res >= 10) {
-            i++;
-            res = res / 10;
-        };
-        if (shrinks == 1) {
-            printf("Case %d: %d\n", num, 1);
-        }
-        else {
-            printf("Case %d: %d\n", num, i + 1);
-        }
-        num++;
+int main(){
+    int cir = 1, n;
+    while(scanf_s("%d", &n) != EOF){
+        int D_p = 1 + floor((n + 1) * log10(3) - n * log10(2));
+        printf("Case %d: %d\n", cir, D_p);
+        cir++;
     }
-    return 0;
 }
