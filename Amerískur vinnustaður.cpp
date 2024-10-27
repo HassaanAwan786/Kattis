b2dc29362ba06cb501f1d88932ea334e5e4c679a
M Hassaan Awan <96558549+HassaanAwan786@users.noreply.github.com>
2024-10-27 19:34:55 +0500

    solved

--- Amerískur vinnustaður.cpp
+++ Amerískur vinnustaður.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
+#include <iomanip>  // Include the <iomanip> header for setprecision
 using namespace std;
-#include <string>
 
 int main() {
     long long fi;
-    cin>>fi;
-    cout<<fi*(0.09144);
+    double ans;
+    
+    cin >> fi;
+    ans = fi * (0.09144);
+    
+    // Set the precision to display more decimal places
+    cout << fixed << setprecision(10) << ans << endl;
 
     return 0;
 }
