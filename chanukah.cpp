c1ebef493d40755a6ebc1ff7f33603879a0603bd
M Hassaan Awan <96558549+HassaanAwan786@users.noreply.github.com>
2024-10-27 20:57:58 +0500

    failed

--- chanukah.cpp
+++ chanukah.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 using namespace std;
+
 int main() {
      
-    long num;
-    long k, days,sum=0;
+    int num;
+    int k, days,sum=0;
     cin >> num;
     for (int l = 0; l < num; l++)
     {
         cin >> k >> days;
-        for (int k = days; k >0; k--)
+        for (int m= days; m>0; m--)
         {
-            sum =sum+ k + 1;
+            sum =sum+ m+ 1;
         }
          
             cout << l + 1 << " " << sum;
