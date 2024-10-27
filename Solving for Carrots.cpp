41e06eb02ad024c6e3698c1ffdbc150aa9529138
M Hassaan Awan <96558549+HassaanAwan786@users.noreply.github.com>
2024-10-27 21:49:40 +0500

    Create Solving for Carrots.cpp

---
+++ Solving for Carrots.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    long n, p;
+    string describe;
+    cin >> n >> p;
+    for (int i = 0; i < n; i++)
+    {
+        getline(cin >>ws,describe);
+    }
+     
+     cout << p;
+    cout << endl;
+    return 0;
+}
+
