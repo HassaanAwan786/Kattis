34fe70af75522b18430a3dd17856265be8f347cc
M Hassaan Awan <96558549+HassaanAwan786@users.noreply.github.com>
2024-10-27 21:31:31 +0500

    compiler error

--- Sierpiński Circumference.cpp
+++ Sierpiński Circumference.cpp
@@ -1,10 +1,10 @@
- #include <iostream>
+#include <iostream>
 #include <algorithm>  
 using namespace std;
 
 int main(){
     int cir = 1, n;
-    while(scanf_s("%d", &n) != EOF){
+    while(scanf("%d", &n) != EOF){
         int D_p = 1 + floor((n + 1) * log10(3) - n * log10(2));
         printf("Case %d: %d\n", cir, D_p);
         cir++;
